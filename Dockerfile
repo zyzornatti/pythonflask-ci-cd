bd8d3fdb03544f28719bb0afad2f8b1776bc78f5
AbdulAzeez Animashaun <tobimustapha53@gmail.com>
2026-05-27 15:40:25 +0100

    updated the pipeline

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
-FROM python:3.10-slim
+FROM python:3.11-slim
 
 WORKDIR /app
 
 COPY requirements.txt .
-RUN pip install -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
+
+EXPOSE 5000
 
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "CI/CD Pipeline Working!"
+    return jsonify({"message": "Hello, from CI/CD"})
 
-app.run(host="0.0.0.0", port=80)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
